0cdf0c96295853bbb0ed9b6a104be7c839c8b017
brucewayne <batman@wayne>
2021-03-04 09:30:42 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -71,3 +71,33 @@ guesses.style.backgroundColor = "yellow";
 guesses.style.fontSize = "80%";
 guesses.style.padding = "15px";
 guesses.style.boxShadow = "3px 3px 6px black";
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Number guessing is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'yo, motherfucker, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Number guessing is cool, ${myName}`;
+  }
+}
