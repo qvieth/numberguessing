06dac7eee398e009bc84b32d36fc7fdfba9944f2
brucewayne <batman@wayne>
2021-03-04 09:23:38 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,73 @@
-let myImage = document.querySelector('img');
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src','images/firefox2.png');
-    } else {
-      myImage.setAttribute('src','images/firefox-icon.png');
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+guessField.focus();
+function checkGuess() {
+    let userGuess = Number(guessField.value);
+    if (guessCount === 1) {
+        guesses.textContent = "Previous guesses: ";
     }
-}
+    guesses.textContent += userGuess + " ";
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+    if (userGuess === randomNumber) {
+        lastResult.textContent = "Congratulations! You got it right!";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = "";
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = "!!!GAME OVER!!!";
+        setGameOver();
+    } else {
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = "Last guess was too low!";
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = "Last guess was too high!";
+        }
+    }
 
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
 }
+guessSubmit.addEventListener("click", checkGuess);
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Start new game";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame);
+}
+function resetGame() {
+    guessCount = 1;
 
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'yo, motherfucker, ' + storedName;
-}
+    const resetParas = document.querySelectorAll(".resultParas p");
+    for (let i = 0; i < resetParas.length; i++) {
+        resetParas[i].textContent = "";
+    }
 
-myButton.onclick = function() {
-  setUserName();
-}
+    resetButton.parentNode.removeChild(resetButton);
 
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
-  }
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = "";
+    guessField.focus();
+
+    lastResult.style.backgroundColor = "white";
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
 }
+guesses.style.backgroundColor = "yellow";
+guesses.style.fontSize = "80%";
+guesses.style.padding = "15px";
+guesses.style.boxShadow = "3px 3px 6px black";
